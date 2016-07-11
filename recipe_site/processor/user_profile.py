189a4d925627c090a85f0f8f870364b635a5b271
Arielle Goldman <arielle745@hotmail.com>
2016-07-11 17:51:54 -0400

    Fixed: context processor defaults to none if there is no logged in user

--- recipe_site/processor/user_profile.py
+++ recipe_site/processor/user_profile.py
@@ -4,8 +4,10 @@ from recipe_site.serializers.user_profile import UserProfileSerializer
 
 def user_profile_context(request):
 
-    user_profiles = UserProfile.objects.filter(user=request.user)
-    current_user_profile = UserProfileSerializer(user_profiles,
-                                                context={'request': request}, many=True).data
+    current_user_profile = None
+    if request.user.is_authenticated():
+        user_profiles = UserProfile.objects.filter(user=request.user)
+        current_user_profile = UserProfileSerializer(user_profiles,
+                                                    context={'request': request}, many=True).data
 
     return {'users': current_user_profile}
